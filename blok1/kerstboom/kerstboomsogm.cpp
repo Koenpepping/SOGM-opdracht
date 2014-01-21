e1c1964624b349f136b117ee128e8cc691649743
KoenPepping <vampie2705@hotmail.com>
2014-01-21 23:41:04 +0100

    kerstboom met ballen
    s Please enter the commit message for your changes. Lines starting

--- blok1/kerstboom/kerstboomsogm.cpp
+++ blok1/kerstboom/kerstboomsogm.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "kerstboomsogm.h"
-
+#include <stdlib.h>
 using namespace std;
 
 
@@ -13,13 +13,22 @@ int aantalkerstbomen=0;
 	
 	cout << "aantal kerstbomen: " << (aantalkerstbomen+aantalkerstbomen) << endl;
 
+int rnd;
+srand (time(NULL));
+
 	for(int boom=0;boom<aantalkerstbomen; ++boom) {
 		for(int rij=0; rij<HOOGTE; ++rij) {    
 			for(int spat=HOOGTE; spat>rij; --spat) {
 				cout << " ";
 			}
 			for(int ster=0 ;ster<(rij*2+1); ++ster) {
-				cout << "*";
+				rnd = rand() % 10 + 1;
+				if (rnd < 2) {
+					cout << "0";
+				}
+				else { 
+					cout << "*";
+				}
 			}
 			for(int spat=HOOGTE; spat>rij; --spat) {
 				cout << " ";
@@ -28,7 +37,13 @@ int aantalkerstbomen=0;
 				cout << " ";
 			}
 			for(int ster=0 ;ster<(rij*2+1); ++ster) {
-				cout << "*";
+				rnd = rand() % 10 + 1;
+				if (rnd < 2) {
+					cout << "0";
+				}
+				else { 
+					cout << "*";
+				}
 			}
 			for(int spat=HOOGTE; spat>rij; --spat) {
 				cout << " ";
