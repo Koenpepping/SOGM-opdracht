97344187bdda8fc5961b3ed7bc49b379081438eb
KoenPepping <vampie2705@hotmail.com>
2014-04-15 19:13:06 +0200

    sinperiode updated to 1 period

--- blok1/sinus/sinperiode.cpp
+++ blok1/sinus/sinperiode.cpp
@@ -8,10 +8,11 @@ using namespace std;
 int F = 480;
 int SR = 48000;
 int phase = 0;
+int length = (SR / F);
 
 int main()
 {
-	for(int i = 0; i <= F; i++)
+	for(int i = 0; i <= length; i++)
 	{
 		double y = sin(i*2*M_PI*F/SR + phase);
 		cout << i << " " << y << endl;
