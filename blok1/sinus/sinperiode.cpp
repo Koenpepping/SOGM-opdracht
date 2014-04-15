fb3fc12daeb0b70e73845ec557eaadbbf6cc2586
KoenPepping <vampie2705@hotmail.com>
2014-04-15 19:02:01 +0200

    sinperiod added

---
+++ blok1/sinus/sinperiode.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "math.h"
+
+#define _USE_MATH_DEFINES
+
+using namespace std;
+
+int F = 480;
+int SR = 48000;
+int phase = 0;
+
+int main()
+{
+	for(int i = 0; i <= F; i++)
+	{
+		double y = sin(i*2*M_PI*F/SR + phase);
+		cout << i << " " << y << endl;
+	}
+}
