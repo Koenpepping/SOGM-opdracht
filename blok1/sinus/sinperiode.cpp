76e504e98472c676af46b051b949efb21b23909f
KoenPepping <vampie2705@hotmail.com>
2014-05-16 01:07:32 +0200

    triangle square and pulse (not finished yet)

--- blok1/sinus/sinperiode.cpp
+++ blok1/sinus/sinperiode.cpp
@@ -23,10 +23,11 @@ int x;
 
 /* 
 if z = 0 this program will output a sinewave
-if z = 1 this program will output a sawtooth 
+if z = 1 this program will output a sawtooth
+if z = 1 this program will output a square
 */
 
-int  z = 1;  
+int  z = 4;  
 
 int main()
 {
@@ -44,14 +45,70 @@ if(z == 0) //sinewave
 if(z == 1) //saw
 	{
 		for(x = 0; x <= length; x++) 			{
-				double fundamental = (sin(x*2*M_PI*F/SR)); // fundamental harmonic
-
+				double fundamental = (sin(x*2*M_PI*F/SR)); // fundamental sine
 				for(int i = 2; i <= NUM_OF_HARMONICS; i++)
 					{
 						fundamental = (fundamental + ((amp/i)*(sin(x*2*M_PI*(F*i)/SR)))); //adds harmonics to the fundamental sinewave
 						y = fundamental;
 					}
 				cout << x << "\t" << y << endl; 
+
 			}
 	}
+
+if(z == 2) //square
+	{
+		for(x = 0; x <= length; x++) 			{
+				double fundamental = (sin(x*2*M_PI*F/SR)); // fundamental sine
+
+				for(int i = 3; i <= 79; i += 2) // + 2 for odd harmonics only 
+					{
+						fundamental = (fundamental + ((amp/i)*(sin(x*2*M_PI*(F*i)/SR)))); //adds odd harmonics to the fundamental sinewave
+						y = fundamental;
+					}
+				cout << x << "\t" << y << endl; 
+			}
+	}
+
+if(z == 3) //pulse WERKT NOG NIET
+	{
+		for(x = 0; x <= length; x++) 			
+		{
+				double fundamental = (sin(x*2*M_PI*F/SR)); // fundamental sine
+
+				int a = 0;
+
+				for(int i = 2; i <= 14; i++)
+					{
+						if((a % 3) == 2)
+								{
+									i += 1;
+								}	
+														
+						fundamental = (fundamental + ((amp/i)*(sin(x*2*M_PI*(F*i)/SR)))); //adds odd harmonics to the fundamental sinewave
+						y = fundamental;
+							
+						a++;
+						//cout << i << endl;
+					}
+				cout << x << "\t" << y << endl; 					
+		}
+	}
+
+if(z == 4) //triangle nog niet helemaal mooi maar voorlopig goed genoeg
+	{
+		for(x = 0; x <= length; x++) 			
+		{
+				double fundamental = (sin(x*2*M_PI*F/SR)); // fundamental sine
+				int t = -1;
+				for(int i = 3; i <= 127; i += 2)
+					{											
+						fundamental = (fundamental + ((t*(amp/pow(i, 2)))*(sin(x*2*M_PI*(F*i)/SR)))); 
+//adds odd harmonics with amplitude 1/(harmonic^2) to the fundamental sinewave, also the phase of the added harmonics is multiplied by PI done with t = -1
+						y = fundamental;
+					}
+				cout << x << "\t" << y << endl; 					
+		}
+	}
+
 }
