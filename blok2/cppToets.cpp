6e9fe2c6efef1e5c3a40cac5a85bee0e9ff77f2a
KoenPepping <vampie2705@hotmail.com>
2014-05-21 12:54:41 +0200

    cppToets added

---
+++ blok2/cppToets.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+#define lengthOfSequence 8
+
+int numberOFOctaves = 1;
+
+
+//base class
+class Instruments
+{
+	public:
+	Instruments();
+		int makeSequence(int length, int numOfOctaves, int index);
+		int output(int length);
+};
+
+//afgeleide class
+class Synth : public Instruments
+{
+	public:
+	Synth();
+	void setNumberofOctaves(int numOctaves);	 
+};
+
+int main()
+{	
+  srand (time(NULL)); //initialize srand
+
+Synth synth1;
+	synth1.setNumberofOctaves(3);
+	synth1.output(lengthOfSequence);
+
+}
+
+//instruments class:
+
+Instruments::Instruments()
+{
+	
+}
+
+int Instruments::makeSequence(int length, int numOfOctaves, int index) //vul array met 8 random noten binnen numOfOctave
+{
+	int notes[length-1];
+
+	for(int i=0; i<=length-1; i++)
+	{
+	notes[i] = rand() % ((numOfOctaves * 8) + 1);	
+	}
+
+return notes[index];
+}
+
+int Instruments::output(int length) //print array 
+{
+	for(int i=0; i<=length-1; i++)
+	{
+		Instruments synth1;
+			cout <<	synth1.makeSequence(lengthOfSequence, numberOFOctaves , i) <<endl;
+	}
+
+return 0;
+}
+
+//synth class afgeleid
+
+Synth::Synth()
+{
+
+}
+
+void Synth::setNumberofOctaves(int numOctaves) // set number of octaves
+{
+ numberOFOctaves = numOctaves;
+}
+
